9b278daa1e8ebc90742084288fb70ac244a01ab0
RishiKaradbhajane <rushikeshk154@gmail.com>
2026-02-23 00:22:03 +0530

    longest consecutive subsequence

---
+++ NeetCode150/longestConsecutiveSubsequence.java
@@ -0,0 +1,30 @@
+package NeetCode150;
+import java.util.*;
+public class longestConsecutiveSubsequence {
+    public static int longestConsecutive(int[] nums){
+        if(nums.length==0) return 0;
+        int maxLen = 0;
+        int len = 0;
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+        for(Integer element : set){
+            int prevEl = element-1;
+            if(!set.contains(prevEl)){
+                len = 1;
+                int nextEl = element + 1;
+                while(set.contains(nextEl)){
+                    len++;
+                    nextEl++;
+                }
+                maxLen = Math.max(maxLen,len);
+            }
+        }
+        return maxLen;
+    }
+    public static void main(String[] args){
+        int n = longestConsecutive(new int[]{1,3,3,4,5,6,8,9,1,2,});
+        System.out.println("Longest consecutive subsequence is " + n);
+    }
+}
